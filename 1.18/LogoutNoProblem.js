8012ae1b898796eb33215bd21aab78e728ecbc52
Polynomial <ricepoly262@gmail.com>
2022-08-06 21:27:16 -0700

    Update LogoutNoProblem.js

--- 1.18/LogoutNoProblem.js
+++ 1.18/LogoutNoProblem.js
@@ -2,6 +2,8 @@
 // Fixes some server hang caused by logging into a base
 
 const logout_filepath = 'kubejs/data/logoutnoproblem.json'
+const DEBUG = 0;
+const log = (str,a) => { if(a||DEBUG){console.log(`[LogoutNoProblem] ${str}`)} } // lol
 
 onEvent("player.logged_out", (event) => {
     let dimension = event.level.dimension.toString()
@@ -19,6 +21,7 @@ onEvent("player.logged_out", (event) => {
     data[event.player.name.string] = logInfo;
     JsonIO.write(logout_filepath, data);
 
+    log(`Sent ${event.player.name.string} to spawn! Original location: ${logInfo}`)
     event.server.runCommandSilent(`cu unstuck ${event.player.name.string}`);
 });
 
@@ -33,6 +36,7 @@ onEvent("player.logged_in", (event) => {
     let logInfo = data[event.player.name.string]
 
     if(logInfo !== undefined){
+        log(`Sent ${event.player.name.string} back to original location: ${logInfo}`)
         event.server.runCommandSilent(`execute in ${logInfo.dim} run tp ${event.player.name.string} ${logInfo.x} ${logInfo.y} ${logInfo.z}`)
     }
 });
